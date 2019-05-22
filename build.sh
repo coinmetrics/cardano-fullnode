8600d013f7acb5e2b691c18cd709eb606ae17499
Quyse Lert <quyse0@gmail.com>
2019-05-22 17:59:04 +0300

    fix image tag

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ export VERSION=$(cat version.txt)
 echo "Image version ${VERSION}."
 
 echo "Building image..."
-docker build --build-arg "VERSION=${VERSION}" -t "${DOCKER_REGISTRY_REPO}:${VERSION}" .
+docker build --build-arg "VERSION=${VERSION}" -t "${DOCKER_REGISTRY_REPO}:${VERSION}_fork" .
 echo "Image ready."
 
 if [ -n "${DOCKER_REGISTRY}" ] && [ -n "${DOCKER_REGISTRY_USER}" ] && [ -n "${DOCKER_REGISTRY_PASSWORD}" ]
 then
 	echo "Logging into ${DOCKER_REGISTRY}..."
 	docker login -u="${DOCKER_REGISTRY_USER}" -p="${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
-	echo "Pushing image to ${DOCKER_REGISTRY_REPO}:${VERSION}..."
-	docker push "${DOCKER_REGISTRY_REPO}:${VERSION}"
+	echo "Pushing image to ${DOCKER_REGISTRY_REPO}:${VERSION}_fork..."
+	docker push "${DOCKER_REGISTRY_REPO}:${VERSION}_fork"
 fi
